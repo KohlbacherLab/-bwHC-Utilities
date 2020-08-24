0347dd8328ffc3677fe5c6720756608834994930
Lucien <lucien.clin@uni-tuebingen.de>
2020-08-24 15:28:27 +0200

    Bit of re-factoring of validation DSL

--- src/main/scala/de/bwhc/util/data/Validation.scala
+++ src/main/scala/de/bwhc/util/data/Validation.scala
@@ -2,6 +2,8 @@ package de.bwhc.util.data
 
 
 
+import java.time.temporal.Temporal
+
 import cats.data.{
   NonEmptyList,
   Validated,
@@ -11,7 +13,6 @@ import Validated.{cond,condNel}
 
 import cats.Traverse
 import cats.syntax.traverse._
-//import cats.syntax.validated._
 
 
 
@@ -21,7 +22,6 @@ object Validation
 
 
   type Validator[E,T] = T => ValidatedNel[E,T] 
-
 
 
   def attempt[T](t: => T): ValidatedNel[Throwable,T] =
@@ -45,17 +45,14 @@ object Validation
   }
 
 
+
   implicit class IterableOps[T, C[X] <: Iterable[X]](val ts: C[T]) extends AnyVal
   {
 
-    def nonEmpty =
-      condNel(!ts.isEmpty,ts,"Iterable empty")
-
-    def ? = nonEmpty
-
-    def ifEmpty[E](err: => E) = (ts ?) otherwise err
+    def ifEmpty[E](err: => E) = condNel(!ts.isEmpty,ts,err)
 
   }
+
 
   //---------------------------------------------------------------------------
   // Traversable
@@ -67,27 +64,12 @@ object Validation
   }
 
 
-
   implicit class OptionOps[T](val opt: Option[T]) extends AnyVal
   {
 
-    def mustBeDefined =
-      condNel(opt.isDefined,opt.get,"Option must be defined")
-
-    def mustNotBeDefined =
-      condNel(!opt.isDefined,opt.get,"Option must not be defined")
-
-//    def defined =
-//      condNel(opt.isDefined,opt.get,"Option undefined")
-
-//    def ? = defined
-
-    def ? = condNel(opt.isDefined,opt.get,"Option undefined")
-
-    def ifUndefined[E](err: => E) = (opt ?) otherwise err 
+    def ifUndefined[E](err: => E) = condNel(opt.isDefined,opt.get,err)
 
   }
-
 
 
   implicit class ValueOps[T](val t: T) extends AnyVal
@@ -96,29 +78,16 @@ object Validation
     def validate[E](implicit v: Validator[E,T]): ValidatedNel[E,T] = v(t)
 
 
-    def mustBeIn(interval: Interval[T]) =
-      condNel(interval contains t, t, s"$t not in $interval")
-
-    def mustBeIn(ts: Iterable[T]) =
-      (ts.find(_ == t) ?) otherwise s"$t not in Iterable collection"
-
-
-    def ifNotIn[E](ts: Iterable[T])(err: => E) = (t mustBeIn ts) otherwise err 
-
-    def ifNotIn[E](i: Interval[T])(err: => E) = (t mustBeIn i) otherwise err 
-
-
-
     //-------------------------------------------------------------------------
     // Ordering Ops
     //-------------------------------------------------------------------------
 
-    def mustBe(v: Validator[String,T]) = v(t)
+//    def mustBe(v: Validator[String,T]) = v(t)
 
-    def mustBe(u: T) =
-      condNel(t == u, t, s"$t not equal to $u")
+//    def mustBe(u: T) =
+//      condNel(t == u, t, s"$t not equal to $u")
 
-
+/*
     def mustBeLess(u: T)(implicit o: Ordering[T]) =
       condNel(o.lt(t,u), t, s"$t not less than $u")
 
@@ -133,77 +102,74 @@ object Validation
 
     def mustBeGreaterOrEq(u: T)(implicit o: Ordering[T]) =
       condNel(o.gteq(t,u), t, s"$t not greater than or equal to $u")
-
+*/
 
   }
 
 
-  object matchers
+  object dsl 
   {
 
-    def be[T](v: Validator[String,T]) = v
+    sealed trait Defined
 
-//    def defined[T]: Validator[String,Option[T]] =
-//      opt => condNel(opt.isDefined,opt,"Option undefined")
-    def defined[T] =
-      (opt: Option[T]) => condNel(opt.isDefined,opt,"Option undefined")
+    val defined = new Defined{}
+
+
+    @annotation.implicitNotFound("${T} is not of type Option[_]")
+    sealed trait IsOption[T]
+    object IsOption
+    {
+      def apply[T](implicit opt: IsOption[T]) = opt
+
+      implicit def option[T] = new IsOption[Option[T]]{} 
+    }
+
 
     def in[T](ts: Iterable[T]) =
-      (t: T) => (ts.find(_ == t) ?) otherwise s"$t not in Iterable collection"
+      (t: T) => condNel(ts.find(_ == t).isDefined, t, s"$t not in Iterable collection")
+
 
     def in[T](interval: Interval[T]) = 
       (t: T) => condNel(interval contains t, t, s"$t not in $interval")
 
-  }
+
+    def lessThan[T](u: T)(implicit ord: Ordering[T]) = 
+      (t: T) => condNel(ord.lt(t,u), t, s"$t not less than $u")
+
+    def <=[T](u: T)(implicit ord: Ordering[T]) = lessThan(u)
+
+    def before[T](u: T)(implicit ord: Ordering[T]) = lessThan(u)
 
 
 
-/*
-  case class ValidatorBuilder[T](t: T) extends AnyVal
-  {
-//    def be = BeValidator(t)
-  }
 
-
-    case class EqualityValidator[T](t: T) extends AnyVal
+    case class MustBe[T](t: T) extends AnyVal
     {
 
-      def apply(u: T) =       
-        condNel(t == u, t, s"$t not equal to $u")
+      def apply(u: T) =
+        condNel(t == u, t, s"$t not equal to $u")    
 
-    }
-
-
-    case class BeValidator[T](t: T) extends AnyVal
-    {
-
-      def less(u: T)(implicit o: Ordering[T]) =
-        condNel(o.lt(t,u), t, s"$t not less than $u")
- 
- 
-      def lessOrEq(u: T)(implicit o: Ordering[T]) =
-        condNel(o.lteq(t,u), t, s"$t not less than or equal to $u")
- 
- 
-      def greater(u: T)(implicit o: Ordering[T]) =
-        condNel(o.gt(t,u), t, s"$t not greater than $u")
- 
- 
-      def greaterOrEq(u: T)(implicit o: Ordering[T]) =
-        condNel(o.gteq(t,u), t, s"$t not greater than or equal to $u")
-
-
-      def in(interval: Interval[T]) =
-        condNel(interval contains t, t, s"$t not in $interval")
       
+      def apply(d: Defined)(implicit opt: IsOption[T]) = 
+        condNel(t.asInstanceOf[Option[Any]].isDefined, t, s"$t is not defined")
 
-      def in(ts: Iterable[T]) =
-        (ts.find(_ == t) ?) otherwise s"$t not in Iterable collection"
-
+      def apply(v: Validator[_,T]) =
+        v(t)
 
     }
 
-*/
+
+    implicit class Syntax[T](val t: T) extends AnyVal
+    {
+
+      def mustBe = MustBe(t)
+
+      def shouldBe = MustBe(t)
+
+    }
+
+  }
+  
 
 
 }
